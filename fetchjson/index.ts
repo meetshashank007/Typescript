7dd0d51fb6564fffff95a0ffa34ec87c30617450
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-09 18:49:59 +0530

    todo interface saftey

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,16 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
   //   console.log(response.data);
-  const todo = response.data;
+  const todo = response.data as ToDo;
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
 
   console.log(`
-        The Todo with ID: ${ID}
+        The Todo with ID: ${id}
         Has a title of: ${title}
         Is it finished? ${finished}
     `);
