936f2361f23832cc7fa215334eb7914955a4cd44
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-09 18:56:33 +0530

    out of order func call

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -14,11 +14,15 @@ axios.get(url).then((response) => {
 
   const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
+  logTodo(id, completed, title);
+});
+
+const logTodo = (id, title, completed) => {
   console.log(`
         The Todo with ID: ${id}
         Has a title of: ${title}
-        Is it finished? ${finished}
+        Is it finished? ${completed}
     `);
-});
+};
