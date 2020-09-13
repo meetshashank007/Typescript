58221f02935d7ccc41a6f3d7cc007f7457714a5a
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-13 19:38:31 +0530

    annotation with functions

---
+++ features/anotations/functions.ts
@@ -0,0 +1,63 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+const devide = (a: number, b: number): number => {
+  return a / b;
+};
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+  //can return null;
+  //can return undefined;
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwErrorB = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+// const logWeather = (forecast: { date: Date; weather: string }): void => {
+//   console.log(forecast.date);
+//   console.log(forecast.weather);
+// };
+
+//ES2015 Destructing
+/*
+const logWeather = ({date, weather}) => {
+  console.log(date);
+  console.log(weather);
+}
+*/
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
