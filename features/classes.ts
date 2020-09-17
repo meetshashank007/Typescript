0f127c4dbb93e55a2efef9736f0809c0b07543f1
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-17 22:42:22 +0530

    classes with super call with annotation

--- features/classes.ts
+++ features/classes.ts
@@ -1,4 +1,12 @@
 class Vehicle {
+  //   color: string;
+  //   constructor(color: string) {
+  //     this.color = color;
+  //   }
+
+  //shorthand
+  constructor(public color: string) {}
+
   //modfifires in action
   //   drive(): void {
   //     console.log('chugga chugga');
@@ -11,6 +19,7 @@ class Vehicle {
   }
 }
 
+/*
 class Car extends Vehicle {
   private drive(): void {
     console.log('vroom');
@@ -30,4 +39,29 @@ const car = new Car();
 // car.drive();
 // car.honk();
 
+car.startDrivingProcess();
+*/
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log('vroom');
+  }
+  public startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+// const vehicle = new Vehicle();
+
+// vehicle.drive();
+// vehicle.honk();
+
+const car = new Car(4, 'red');
 car.startDrivingProcess();
