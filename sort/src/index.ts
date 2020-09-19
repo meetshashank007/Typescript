a2bef987ba71666a7ff01d6aebb86430c70164f1
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-19 19:34:37 +0530

    working with type guard in case of multiple types

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,28 @@
-console.log('Hi, there', 123);
+class Sorter {
+  constructor(public collection: number[] | string) {}
+  sort(): void {
+    const { length } = this.collection;
 
-const logSomething = () => {
-  console.log('something');
-};
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        //restore the union inpact on type
+        //type guard for array to uplift the readonly on number
+        if (this.collection instanceof Array) {
+          // collection === number[]
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+        //case for string
+        if (typeof this.collection === 'string') {
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
