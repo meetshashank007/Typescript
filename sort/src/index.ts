ba6678d39893ad845c75b73d1028a02e6db52c32
Dev Shashank Gupta <devshashank@pop-os.localdomain>
2020-09-23 19:39:37 +0530

    number sorter

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,28 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    const { length } = this.collection;
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        //restore the union inpact on type
-        //type guard for array to uplift the readonly on number
-        if (this.collection instanceof Array) {
-          // collection === number[]
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-        //case for string
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
+const numberCollection = new NumbersCollection([10, 3, -10, -5, 0]);
 
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection);
